a504a68a9f1be84f0fb1d7f324de5c63668df38e
Jesper Brännström <jesper.brannstrom@gmail.com>
2026-04-29 16:25:14 +0900

    Fixed merge conflicts

--- src/app/app_runtime.cpp
+++ src/app/app_runtime.cpp
@@ -1,5 +1,15 @@
 #include "app/app_runtime.h"
 
+#ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#endif
+
 #include "app/native_file_dialogs.h"
 
 #include "ai/ai_claim_review.h"
